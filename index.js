1ea116642755cfe33bcc70b4636adca925bfd5d9
Christian Reyna <christianreyna12@gmail.com>
2023-08-25 10:10:34 -0500

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = []; 
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`; 
+      messages.push(message); 
+    }
+  
+    return messages;
+  }
+  
+
+  const names = ["Charlie", "Samip", "Ali"];
+  const eventName = "birthday";
+  const thankYouMessages = writeCards(names, eventName);
+  console.log(thankYouMessages);
+
+  function countDown(startingNumber) {
+    let currentNumber = startingNumber; 
+  
+    while (currentNumber >= 0) { 
+      console.log(currentNumber); 
+      currentNumber--;
+    }
+  }
+  
+
+  countDown(10); 
